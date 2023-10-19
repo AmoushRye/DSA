ef2bf1487ed27a53e6cfd4542fd8b67062fca026
AmoushRye <111222569+AmoushRye@users.noreply.github.com>
2023-10-19 20:53:32 +0530

    Update DFSTraversal

--- Graphs/DFSTraversal.cpp
+++ Graphs/DFSTraversal.cpp
@@ -1,1 +1,14 @@
-
+void dfs(vector<int> adj[],vector<int> &vis,vector<int> &ans,int start){
+        ans.push_back(start);
+        vis[start] = 1;
+        for(auto itr : adj[start]){
+            if(vis[itr] == 0)dfs(adj,vis,ans,itr);
+        }
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<int> vis(V,0);
+        vector<int> ans;
+        dfs(adj,vis,ans,0);
+        return ans;
+    }
