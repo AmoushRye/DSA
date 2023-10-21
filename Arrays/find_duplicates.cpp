025a639517429c8336b48b7e46bc9c856ac91431
AmoushRye <111222569+AmoushRye@users.noreply.github.com>
2023-10-21 17:11:57 +0530

    Update find_duplicates

--- Arrays/find_duplicates.cpp
+++ Arrays/find_duplicates.cpp
@@ -17,3 +17,26 @@ int main(){
     cout << duplicates(arr);
     return 0;
 }
+
+//alternative and better
+
+    //remember the array element values should be less than n(size of array)
+    //and atleast one element should be repeated.
+
+    //basically we are manipulating or changing the elements value by adding n to it 
+    //if there are same elements they will be definitely pointing to the same index
+    //by this formula a[a[i%n]] and on adding n 
+    //there will be a time when that n will be added twice to that array index element
+    //and we check for that and hence that will be the duplicate element.
+    int findDuplicate(vector<int>& a) {
+        int n = a.size();
+        for(int i = 0;i<n;i++){
+            a[a[i]%n]+=n;
+        }
+        for(int i = 0;i<n;i++){
+            if(a[i]>=n*2){
+                return i;
+            }
+        }
+        return -1;
+    }
